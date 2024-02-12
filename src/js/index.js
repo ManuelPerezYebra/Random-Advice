ea98e477311036d062bd26db8410a0b10ccca030
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-02-12 19:06:20 +0100

    Finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
-import { sayHello } from './demo';
+const adviceIdElement = document.getElementById('idNumber');
+const randomAdviceElement = document.getElementById('randomAdvice');
+const adviceButtonElement = document.getElementById('adviceButton');
 
-sayHello();
+let phrase;
+
+const getPhrase = async () => {
+	const response = fetch('https://api.adviceslip.com/advice');
+	const data = await (await response).json();
+	phrase = data;
+	console.log(data);
+	fillRandomAdvice();
+};
+
+const fillRandomAdvice = () => {
+	randomAdviceElement.textContent = phrase.slip.advice;
+	adviceIdElement.textContent = phrase.slip.id;
+};
+
+adviceButtonElement.addEventListener('click', getPhrase);
